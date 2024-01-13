cc230dcb761533f4c54c9e000ce274384f6d896b
sojanonelson <sojanonelson54@gmail.com>
2024-01-13 12:35:33 +0530

    corretions

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -10,11 +10,12 @@ import profile from "../../images/profile.jpeg";
 
 function Navbar() {
   return (
-    <div className='flex bg-[#121212] w-full h-16 mt-1 rounded-tl-md rounded-tr-md justify-center items-center'>
+    <div className='flex bg-[#121212] w-full h-auto mt-1 rounded-tl-md rounded-tr-md justify-center items-center'>
 
       <nav className="content-navbar mb-3 p w-full">
         <div className="md:flex md:items-center">
-          <div className="md:w-3/3 flex ">
+          <div className='hidden md:block'>
+          <div className="md:w-3/3 flex flex-col  md:block bg-[#121212]  ">
             <FontAwesomeIcon
               className="ml-1 mt-3 cursor-pointer rounded-full p-2 bg-black"
               color="#a7a7a7"
@@ -26,13 +27,14 @@ function Navbar() {
               icon={faChevronRight}
             ></FontAwesomeIcon>
           </div>
+          </div>
           <div className="md:w-3/3 px-2 mt-3">
             <input
-              className="content-main-nav-input bg-[#242424] ml-4 w-full md:w-[350px] h-9 p-6 rounded-full"
+              className="content-main-nav-input bg-[#242424]  w-full md:w-[350px] h-9 p-6 rounded-full"
               placeholder="What do you want to listen to?"
             ></input>
           </div>
-          <div className="md:w-2/3 flex justify-end items-center mt-3">
+          <div className="md:w-2/3 bg-[#121212]  flex justify-end items-center mt-3">
             <button className="bg-white text-black text-sm px-3 font-medium hover:h-10 hover:text-[0.900rem] transition duration-300 h-9 rounded-full content-navbar-right">
               Explore Premium
             </button>
@@ -46,7 +48,7 @@ function Navbar() {
             ></FontAwesomeIcon>
             <button>
               <img
-                className=" w-8 mx-1 rounded-full"
+                className=" w-8 mx-1 bg-[#121212]  rounded-full"
                 src={profile}
                 alt="profile"
               ></img>
